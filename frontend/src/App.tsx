72e8d602259bdb91dec3757f4d40a8598a686093
NicolasNakasone <nicolasnakasone70@gmail.com>
2021-08-06 23:35:33 -0300

    Changes made up so far

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Pokemon</h1>
+      <h1>Find The Tech Question</h1>
     </div>
   );
 }
---
+++ frontend/src/index.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable import/extensions */
+// /* eslint-disable no-unused-vars */
+/* eslint-disable prefer-const */
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
