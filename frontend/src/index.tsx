5462832908942eebb3be3624d39a607fede14d7f
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2022-12-10 04:03:00 -0300

    fix(eslint): modified eslint rules from frontend folder
    * Also it has been deleted some unused files in frontend folder

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,15 @@
 /* eslint-disable import/extensions */
 // /* eslint-disable no-unused-vars */
 /* eslint-disable prefer-const */
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import { StrictMode } from 'react'
+
+import ReactDOM from 'react-dom'
+
+import App from './App'
 
 ReactDOM.render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
