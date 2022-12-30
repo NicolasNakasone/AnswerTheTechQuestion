4d13f844cf75ab5415f4bd5362f7c7f48d1b239c
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2022-12-30 18:56:33 -0300

    feat(dialog): created first functional dialog component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
+import { useState } from 'react'
+
+import { Dialog } from 'src/components/common/Dialog'
+
 export const App = () => {
+  const [open, setOpen] = useState(false)
   return (
     <div className="App">
       <h1>Find The Tech Question</h1>
+      <Dialog {...{ open }}>
+        <p>hola</p>
+        <button onClick={() => setOpen(false)}>x</button>
+      </Dialog>
+
+      <button onClick={() => setOpen(!open)}>
+        {!open ? 'abrir' : 'cerrar'}
+      </button>
     </div>
   )
 }
