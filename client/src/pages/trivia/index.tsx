7d349f3a9e3a4447a445f0e2791f7bebd360ed6a
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2023-09-18 19:17:08 -0300

    fix(file): modified file name

--- client/src/pages/trivia/index.tsx
+++ client/src/pages/trivia/index.tsx
@@ -4,7 +4,7 @@ import { Box } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 import { QuestionCard } from 'src/feature/trivia/QuestionCard'
 import { TriviaResult } from 'src/feature/trivia/TriviaResult'
-import { shuffledTrivia } from 'src/mocks/mockTrivias'
+import { shuffledTrivia } from 'src/mocks/trivias.mock'
 
 export const Trivia = () => {
   const [currentIndex, setCurrentIndex] = useState(0)
