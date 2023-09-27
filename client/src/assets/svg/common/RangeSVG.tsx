4dc238fe2b94b226b682c9ed7862793114e788d6
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2023-09-26 22:27:53 -0300

    fix(name): modified RangeSVG file name and rank icons

--- client/src/assets/svg/common/RangeSVG.tsx
+++ client/src/assets/svg/common/RangeSVG.tsx
@@ -14,23 +14,20 @@ export const BasicSVG = ({ isDisabled }: RangeSVG): JSX.Element => {
 
 export const IntermediateSVG = ({ isDisabled }: RangeSVG): JSX.Element => {
   return (
-    <svg height="32" viewBox="0 0 24 32" fill={!isDisabled ? '#FFA741' : '#E2E8F0'}>
-      <rect width="4" height="20" x="2" y="12" />
-      <rect width="4" height="24" x="8" y="8" />
-      <rect width="4" height="28" x="14" y="4" />
-      <rect width="4" height="32" x="20" y="0" />
+    <svg height="28" viewBox="0 0 18 28" fill={!isDisabled ? '#FFA741' : '#E2E8F0'}>
+      <rect width="4" height="20" x="2" y="8" />
+      <rect width="4" height="24" x="8" y="4" />
+      <rect width="4" height="28" x="14" y="0" />
     </svg>
   )
 }
 
 export const AdvancedSVG = ({ isDisabled }: RangeSVG): JSX.Element => {
   return (
-    <svg height="52" viewBox="0 0 30 52" fill={!isDisabled ? '#FF7777' : '#E2E8F0'}>
-      <rect width="4" height="36" x="2" y="16" />
-      <rect width="4" height="40" x="8" y="12" />
-      <rect width="4" height="44" x="14" y="8" />
-      <rect width="4" height="48" x="20" y="4" />
-      <rect width="4" height="52" x="26" y="0" />
+    <svg height="40" viewBox="0 0 18 40" fill={!isDisabled ? '#FF7777' : '#E2E8F0'}>
+      <rect width="4" height="32" x="2" y="8" />
+      <rect width="4" height="36" x="8" y="4" />
+      <rect width="4" height="40" x="14" y="0" />
     </svg>
   )
 }
