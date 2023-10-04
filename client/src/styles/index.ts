1bffe664e1ee23e1eee1edf3e45c50f294bbbc53
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2023-10-04 15:45:33 -0300

    feat(colors): added app colors

--- client/src/styles/index.ts
+++ client/src/styles/index.ts
@@ -1,7 +1,41 @@
 import { extendTheme } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools'
+
+export interface Colors {
+  primary: string
+  primaryLight: string
+  primaryDark: string
+  textLight: string
+  textDark: string
+  backgroundLight: string
+  backgroundDark: string
+  disabled: string
+  shadows: string
+}
+
+const colors = {
+  primary: '#A991AE',
+  primaryLight: '#EBB9DF',
+  primaryDark: '#4D3B56',
+  textLight: '#F8F4E3',
+  textDark: '#330C2F',
+  backgroundLight: '#F6F3F6',
+  backgroundDark: '#2D232F',
+  disabled: '#EDE8EE',
+  shadows: '#67697C',
+}
 
 export const theme = extendTheme({
   fonts: {
     body: 'Kanit, Montserrat, sans-serif',
+    colors,
+  },
+  styles: {
+    global: (props: any) => ({
+      body: {
+        color: mode(colors.textDark, colors.textLight)(props),
+        bg: mode(colors.backgroundLight, colors.backgroundDark)(props),
+      },
+    }),
   },
 })
