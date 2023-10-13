60bed64df1d5853bd1119244ed190036d673d56d
Nicolas Nakasone <nicolasnakasone70@gmail.com>
2023-10-13 02:32:44 -0300

    feat(config): finished initial configurations for app backend
    * It has been modified .eslintrc.json file
    
    * It has been modified tsconfig.json file
    
    * Now imports inside api folder accepts absolute paths

---
+++ api/src/app.ts
@@ -0,0 +1,36 @@
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+import express, { NextFunction, Request, Response } from 'express'
+import morgan from 'morgan'
+
+dotenv.config()
+
+const server = express()
+const { API_PORT } = process.env
+
+server.set('port', API_PORT || 4001)
+server.use(express.urlencoded({ extended: true, limit: '50mb' }))
+server.use(express.json({ limit: '50mb' }))
+server.use(cookieParser())
+server.use(morgan('dev'))
+server.use((req: Request, res: Response, next: NextFunction) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000') // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  next()
+})
+
+// server.use('/', routes)
+
+// Error catching endware.
+server.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  // @ts-expect-error Type Error does not include status
+  const status = err.status || 500
+  const message = err.message || err
+  console.error(err)
+  res.status(status).send(message)
+  next()
+})
+
+export default server
